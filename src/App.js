71c821930d3c156f2c31b893cb7efcde5e44537a
Christopher Tso <christso@gmail.com>
2019-06-12 20:38:47 +1000

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar';
-import ProductRow from './ProductRow';
-import ProductCategoryRow from './ProductCategoryRow';
 import ProductTable from './ProductTable';
 
 const PRODUCTS = [
